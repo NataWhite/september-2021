63800a1de06e026d6735fc205461f2fc2dd3d1b2
NataWhite <nata.ivanova.one@gmail.com>
2022-06-02 22:40:37 +0300

    lesson 1

---
+++ src/user/user.controller.ts
@@ -0,0 +1,34 @@
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+} from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UserService } from './user.service';
+
+@Controller('users')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @HttpCode(HttpStatus.OK)
+  @Get('/')
+  getAll() {
+    return this.userService.getAllUsers();
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Get('/:id')
+  getOneUserById(@Param('id') id: string) {
+    return this.userService.getOneById(id);
+  }
+
+  @HttpCode(HttpStatus.CREATED)
+  @Post()
+  createUser(@Body() userDto: CreateUserDto) {
+    return this.userService.createUser(userDto);
+  }
+}
---
+++ src/user/user.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+
+@Injectable()
+export class UserService {
+  private users = [];
+
+  getAllUsers() {
+    return this.users;
+  }
+
+  getOneById(id: string) {
+    return this.users.find((user) => user.id == id);
+  }
+
+  createUser(userDto: CreateUserDto) {
+    this.users.push({
+      ...userDto,
+      id: new Date().valueOf(),
+    });
+    return userDto;
+  }
+}
